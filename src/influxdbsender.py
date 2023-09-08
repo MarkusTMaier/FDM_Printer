855f41919e6ae093c0615ce9e03850ac576df5a4
Markus Maier <markus.maier@inspire.ch>
2023-09-08 18:31:04 +0200

    BUgFix INFLUXDB after UMATI

--- src/influxdbsender.py
+++ src/influxdbsender.py
@@ -29,7 +29,7 @@ class DataCollectorAndSender:
         self.loop_sleep = config.getfloat('SendDataSettings', 'loop_sleep')
 
         self.value_ranges = [
-        6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009, 6010, 6011, 6012, 6013, 6014,
+        6001, 6002, 6003, 6004, 6005, 6007, 6013, 6014,
         6015, 6016, 6017, 6018, 6019, 6020, 6021, 6022, 6023, 6024, 6025, 6026,
         6027, 6028, 6029, 6030, 6031, 6032, 6033, 6034, 6035, 6036, 6037, 6038, 6039,
         6040, 6041, 6042, 6043, 6044, 6045, 6046, 6047, 6048, 6049, 6050, 6052, 6053, 6054, 6055,
@@ -70,7 +70,7 @@ class DataCollectorAndSender:
                 "unit": "nounit",
                 "value": values.get(f"value{i}"),
             }
-            for r in self.value_ranges for i in r
+            for i in self.value_ranges
         }
 
     async def main(self):
