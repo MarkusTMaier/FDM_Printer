bbede411f62a3f8ae90cc0b303b7f36eb933a288
mvincent-ethzBA <matteo.vincent@outlook.com>
2024-09-04 14:22:57 +0200

    updated bluepy to 1.3.0, should fix things

--- src/influx_and_sem6000controller.py
+++ src/influx_and_sem6000controller.py
@@ -41,7 +41,7 @@ while True:
         p = influxdb_client.Point("value6172").tag("printer_id", "MK3S").field("Wattage", wattage)
         write_api.write(bucket=bucket, org=org, record=p)
 
-    except (SEMSocket.NotConnectedException, BrokenPipeError):
+    except (SEMSocket.NotConnectedException, bluepy.btle.BTLEDisconnectError, BrokenPipeError):
         print("Restarting...")
         if socket != None:
             socket.disconnect()
