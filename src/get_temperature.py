5f23486030c40a9bdb13b32600de3c6056b3bb1b
mvincent-ethzBA <matteo.vincent@outlook.com>
2024-09-05 10:57:35 +0200

    bugfix

--- src/get_temperature.py
+++ src/get_temperature.py
@@ -4,6 +4,7 @@ import time
 i = 0
 while i < 10:
     temperature = librtd.get(0,7)
+    temperature = round(temperature, 2)
     i = i+1
     print(f"The temperature is currently {temperature} °C")
     time.sleep(5)
